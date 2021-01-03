1081eaee75d0a04ede6fcc82317f391976423807
Ryuichi <ne191008@senshu-u.jp>
2021-01-03 08:42:25 +0000

    View(navi)まわり

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,9 @@
 class TopController < ApplicationController
+
+def index
+end
+
+def login
+end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def page_title
+        title = "Sirius"
+        title = @page_title + "-" + title if@page_title
+        title
+    end
+
+    def menu_link_to(text, path, options = {})
+        content_tag :li do
+            condition = options[:method] || !current_page?(path)
+            
+            #リンク先が現ページ
+            link_to_if(condition,text, path, options) do
+                #txtをspanタグで囲む
+                content_tag(:span, text)
+            end
+        end
+    end
 end
