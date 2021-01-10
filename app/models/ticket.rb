7de081c2f13db04c096be66000ccac3d211eefd2
Ryuichi <ne191008@senshu-u.jp>
2021-01-10 18:17:59 +0000

    ticket在庫管理, view, validate

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,15 @@
 class Ticket < ApplicationRecord
 
+    #Association
     belongs_to :event
     belongs_to :member
+
+    #Validate
+    validate do
+        #在庫数とチケット数
+        if event.tickets.count >= event.capacity
+            errors.add(:base, :invalid)
+        end
+    end
     
 end
