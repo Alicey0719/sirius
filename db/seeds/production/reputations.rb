906a631fa50a75ac270438c4196ff45a894be4ab
Ryuichi <ne191008@senshu-u.jp>
2021-01-10 16:30:35 +0000

    seed production

--- db/seeds/production/reputations.rb
+++ db/seeds/production/reputations.rb
@@ -1,10 +1,28 @@
-0.upto(9) do |idx|
-  id = rand(1..10)
-  while id == (idx+1)
-    id = rand(1..10)
-  end
+#Platina
+30.times{ |t|
   Reputation.create(
-    liked_member_id: idx+1,
-    like_member_id: id
-  )
-end
+    event: Event.find(5),
+    member: Member.find(t+1)
+    )
+}
+#Gold
+20.times{ |t|
+  Reputation.create(
+    event: Event.find(4),
+    member: Member.find(t+1)
+    )
+}
+#Silver
+10.times{ |t|
+  Reputation.create(
+    event: Event.find(3),
+    member: Member.find(t+1)
+    )
+}
+#bronze
+5.times{ |t|
+  Reputation.create(
+    event: Event.find(2),
+    member: Member.find(t+1)
+    )
+}
